5cc3c4b62a5f45947c6c34fd8d4118c16d598053
evans351 <evansotieno3617@gmail.com>
2022-12-12 01:09:33 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,14 @@
 
 /**
  * main - print "Programming is like building a multilingual puzzle
- * followed by a new line.
- *
- * Return Always 0.
+ * followed, by a new line.
+ * Return: Always 0.
  */
 
-int main(void)
+int maim(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle")
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
