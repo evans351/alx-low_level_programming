4ba68dd2a7d2b3c2541d05b67738741832cba46e
evans351 <evansotieno3617@gmail.com>
2022-12-12 02:55:01 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("With proper grammar, but the outcome is apiece of art");
+	return (0);
+
+}
